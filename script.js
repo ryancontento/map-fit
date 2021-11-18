5e86b8f74034bcd8cdba8773b4434534869f964d
Ryan Contento <contento.rk@gmail.com>
2021-11-18 06:49:41 -0500

    form improvements (actually working ok)

--- script.js
+++ script.js
@@ -43,6 +43,14 @@ if (navigator.geolocation)
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
+
+  // clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
   // display marker
   console.log(mapEvent);
   const { lat, lng } = mapEvent.latlng;
