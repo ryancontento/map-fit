eca97cf9795578a94eb65913ee278510775fc3c6
Ryan Contento <contento.rk@gmail.com>
2021-11-20 16:58:22 -0500

    setting up workout object

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  constructor(coords, )
+}
+
 class App {
   #map;
   #mapEvent;
